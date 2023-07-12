b009bd15ae16a4a6c728c9c8fe0f86b592b5a8ba
unknown <simon castillo>
2023-07-12 17:26:13 +0200

    v1.0 archivos base

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+    include "./templates/header.php";
+?>
+     <header>
+        <nav>
+            <ul>
+                <li><a href="#">Ver Perfil</a></li>
+                <li><a href="#">Cerrar Sesión</a></li>
+            </ul>
+        </nav>
+     </header>
+     <h1>Bienvenido</h1>
+<?php 
+    include "./templates/footer.php";
+?>
